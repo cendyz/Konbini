65f1bcb1a2961f5a00cd61f7ff9f38e766e1e97a
cendyz <252937228+cendyz@users.noreply.github.com>
2026-07-20 11:05:34 +0200

    utils testing

--- tests/utilsTests.cpp
+++ tests/utilsTests.cpp
@@ -2,12 +2,26 @@
 #include "../include/Utils.h"
 
 
-TEST(isNumber, RecognizesVariousFormatsCorrectly) {
+TEST(isNumber, RecognizesVariousFormatsCorrectly)
+{
     EXPECT_TRUE(Utils::isInt("93"));
     EXPECT_TRUE(Utils::isInt("3"));
-    EXPECT_TRUE(Utils::isInt(""));
+    EXPECT_FALSE(Utils::isInt(""));
     EXPECT_FALSE(Utils::isInt(" 2"));
     EXPECT_FALSE(Utils::isInt("78 "));
     EXPECT_FALSE(Utils::isInt("ww"));
     EXPECT_FALSE(Utils::isInt("ske$2a"));
+}
+
+TEST(isLowering, ChangeBigCharsToLow)
+{
+    std::array<std::string, 4> strs{"AWER", "aWe4R", "", " ra R9 4aAb"};
+    const std::array<std::string, 4> exps{"awer", "awe4r", "", " ra r9 4aab"};
+
+    for (size_t i{}; i < 4; ++i)
+    {
+        Utils::lowerString(strs[i]);
+        EXPECT_EQ(strs[i], exps[i]);
+    }
+
 }
